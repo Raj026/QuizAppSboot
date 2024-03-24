3c8308534e519be7294e1b74b2aeff28b5ea9b6c
Raj026 <rajpatani31@gmail.com>
2024-03-24 14:10:56 +0530

    Added Exception Handling according to HTTP status code

--- src/main/java/com/rapa/quizapp/controller/QuestionService.java
+++ src/main/java/com/rapa/quizapp/controller/QuestionService.java
@@ -3,6 +3,9 @@ package com.rapa.quizapp.controller;
 import com.rapa.quizapp.Question;
 import com.rapa.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,4 +21,11 @@ public class QuestionService {
     public List<Question> getQuestionsByCategory(String category) {
         return questionDao.findByCategory(category);
     }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        questionDao.save(question);
+//        return "Saved Successfully";
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/rapa/quizapp/service/QuestionController.java
+++ src/main/java/com/rapa/quizapp/service/QuestionController.java
@@ -3,11 +3,11 @@ package com.rapa.quizapp.service;
 import com.rapa.quizapp.Question;
 import com.rapa.quizapp.controller.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController // This is to make the program understand that this is controller class
@@ -16,15 +16,33 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
-        return questionService.getAllQuestions();
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        try{
+            return new ResponseEntity<>(questionService.getAllQuestions(), HttpStatus.OK);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+        // This will return the response with the 200 Status code and notify the user that the response generated is perfectly working.
+        // We can also do this above thing in the service layer instead of controller layer.
     }
 
     @GetMapping("category/{category}")
     //If you mention {cat} in here then you have to make changes in the path variable
     //i.e.- @Pathvariable("cat") String category
-    public List<Question> getQuestionByCategory(@PathVariable String category) {
-        return questionService.getQuestionsByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category) {
+        try {
+            return new ResponseEntity<>(questionService.getQuestionsByCategory(category), HttpStatus.OK);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
 
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
     }
 }
